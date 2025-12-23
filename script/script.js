5b9b994bc2fde60c85e6f8e44567096379d6197f
Mohsen Zamani <mohsenzamanist@gmail.com>
2025-12-23 17:47:58 +0000

    Add model screen

--- script/script.js
+++ script/script.js
@@ -4,6 +4,8 @@ const main = document.querySelector("main");
 const menuBtn = document.querySelector("#menu__hamburger-button");
 const closeBtn = document.querySelector("#menu__off-screen__close-button");
 const nav = document.querySelector("header nav");
+const modalScreen = document.querySelector("#modal-screen");
+const modalContent = document.querySelector("#modal-screen div");
 
 window.addEventListener("load", () => {
   captions.forEach((caption, i) => {
@@ -11,6 +13,13 @@ window.addEventListener("load", () => {
     figure.innerHTML = `<img src="img/${
       i + 1
     }.jpg" loading="lazy" /><figcaption>${caption}</figcaption>`;
+    figure.addEventListener("click", (e) => {
+      modalContent.innerHTML = `<img src="img/${
+        i + 1
+      }.jpg" style='width:60%;' />`;
+
+      modalScreen.style.visibility = "visible";
+    });
     main.appendChild(figure);
   });
 });
@@ -21,4 +30,12 @@ menuBtn.addEventListener("click", () => {
 
 closeBtn.addEventListener("click", () => {
   nav.classList.remove("open");
+});
+
+modalContent.addEventListener("click", (e) => {
+  e.stopPropagation();
+});
+
+modalScreen.addEventListener("click", () => {
+  modalScreen.style.visibility = "hidden";
 });
