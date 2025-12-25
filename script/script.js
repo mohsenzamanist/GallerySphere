5a6c69ced3f155269b600cce89ac70fa1db7d003
Mohsen Zamani <mohsenzamanist@gmail.com>
2025-12-25 20:31:09 +0000

    Add responsive design

--- script/script.js
+++ script/script.js
@@ -1,11 +1,12 @@
 import { captions } from "./caption.js";
 
 const main = document.querySelector("main");
-const menuBtn = document.querySelector("#menu__hamburger-button");
+const hamburgerBtn = document.querySelector("#menu__hamburger-button");
 const closeBtn = document.querySelector("#menu__off-screen__close-button");
 const nav = document.querySelector("header nav");
 const modalScreen = document.querySelector("#modal-screen");
 const modalContent = document.querySelector("#modal-screen div");
+const menuLinks = document.querySelector("#menu__off-screen__links");
 
 window.addEventListener("load", () => {
   captions.forEach((caption, i) => {
@@ -13,7 +14,7 @@ window.addEventListener("load", () => {
     figure.innerHTML = `<img src="img/${
       i + 1
     }.jpg" loading="lazy" /><figcaption>${caption}</figcaption>`;
-    figure.addEventListener("click", (e) => {
+    figure.addEventListener("click", () => {
       modalContent.innerHTML = `<img src="img/${
         i + 1
       }.jpg" style='width:60%;' />`;
@@ -24,7 +25,7 @@ window.addEventListener("load", () => {
   });
 });
 
-menuBtn.addEventListener("click", () => {
+hamburgerBtn.addEventListener("click", () => {
   nav.classList.add("open");
 });
 
@@ -38,4 +39,16 @@ modalContent.addEventListener("click", (e) => {
 
 modalScreen.addEventListener("click", () => {
   modalScreen.style.visibility = "hidden";
+});
+
+menuLinks.addEventListener("click", (e) => {
+  const clickedItem = e.target.closest(".menu__off-screen__links__link-item");
+
+  menuLinks
+    .querySelectorAll("li")
+    .forEach((li) =>
+      li.classList.remove("menu__off-screen__links__link-item--active")
+    );
+
+  clickedItem.classList.add("menu__off-screen__links__link-item--active");
 });
